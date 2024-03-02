a8cfc610468e21dbc63924a821fef853273dfd76
Ayush5071 <ayusht5071@gmail.com>
2024-03-03 00:49:19 +0530

    created a todo list app

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <>
+    <div className='w-full h-14 bg-black font-semibold text-5xl text-white text-center '>My TODO LIST APP</div>
+    </>
+  )
+}
+
+export default header
